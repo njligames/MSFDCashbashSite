b0b3b9417ae8269b1b4133b54e060ed2a4a7b032
3psprd <3psprd@3rdpartysolutions.com>
2022-12-23 10:21:01 -0500

    fixed the button layout and the button generation script

--- generate_buttons.py
+++ generate_buttons.py
@@ -3,29 +3,36 @@
 
 import csv
 
+available="""
+<button 
+    type="button" 
+    class="btn btn-primary btn-custom" 
+    data-bs-toggle="modal" 
+    data-bs-target="#exampleModal{ticket_number}" 
+    data-whatever="{ticket_number}">Ticket #{ticket_number}
+</button>
+"""
+
+disabled="""
+<button 
+    type="button" 
+    class="btn btn-primary btn-custom" 
+    data-bs-toggle="modal" 
+    data-bs-target="#exampleModal{ticket_number}" 
+    data-whatever="{ticket_number}" 
+    disabled>Ticket #{ticket_number}
+</button>
+"""
 
 with open('tickets.csv') as csvfile:
     reader = csv.DictReader(csvfile)
-    col=1
-    idx=1
+
+    print('<div class="row gap-2">')
     for row in reader:
-        text='<button onclick="initPayPalButton' + str(idx) + '()" type="button" class="btn btn-primary btn-custom" data-bs-toggle="modal" data-bs-target="#exampleModal' + str(idx) + '" data-whatever="{}">Ticket {}</button>'
-        text_disabled='<button type="button" class="btn btn-primary btn-custom" data-bs-toggle="modal" data-bs-target="#exampleModal' + str(idx) + '" data-whatever="{}" disabled>Ticket {}</button>'
+        ticket_number=row["ticket_number"]
 
-        if 1 == col:
-            print('<div class="row justify-content-md-center">')
-
-        print('<div class="col d-grid gap-2">')
         if row['available'] == "true":
-            print(text.format(row['ticket_number'], row['ticket_number']))
+            print(available.format(ticket_number = row['ticket_number']))
         else:
-            print(text_disabled.format(row['ticket_number'], row['ticket_number']))
-        print('</div>')
-
-        col += 1
-        idx += 1
-
-        if 6 == col:
-            print('</div></br>')
-            col = 1
-
+            print(disabled.format(ticket_number = row['ticket_number']))
+    print('</div>')
